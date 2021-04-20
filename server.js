2966e6f82f3e1b0db9b4a3c0b80c0e0e764817b6
HugoWeb23 <hugohourriez@live.be>
2021-04-20 22:27:05 +0200

    Changement port

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const passport = require('passport');
 const cors = require('cors');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
