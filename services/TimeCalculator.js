c29dc33396c2c50197031c189c7d1bdd1d58aff6
HugoWeb23 <hugohourriez@live.be>
2021-06-15 19:27:50 +0200

    Modification de l'affichage du temps des parties

--- services/TimeCalculator.js
+++ services/TimeCalculator.js
@@ -11,7 +11,10 @@ const TimeCalculator = (date1, date2) => {
     let hours = h > 0 ? h + (h == 1 ? " heure, " : " heures, ") : "";
     let minutes = m > 0 ? m + (m == 1 ? " minute, " : " minutes et ") : "";
     let second = s > 0 ? s + (s == 1 ? " seconde" : " secondes") : "";
-    return days + hours + minutes + second;
+    if(d > 7) {
+        return "Plus de 7 jours"
+    }
+    return days + hours + minutes + (d > 0 ? "" : second);
     }
 
 module.exports = TimeCalculator
